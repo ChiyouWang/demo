9c31858f2d0bda311956c1e18ccd5bc4151a67c5
tmh0214 <smyy0214@163.com>
2017-06-15 16:12:12 +0800

    dev push 2

--- src/cn/tmh/demo/demo.java
+++ src/cn/tmh/demo/demo.java
@@ -5,7 +5,7 @@ package cn.tmh.demo;
  */
 public class demo {
     public static void main(String[] args) {
-        System.out.print("dev push");
+        System.out.print("dev push2");
 
     }
 }
